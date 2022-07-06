e4cae5a885fe5cb1f8a497739c1e4664cbdc9962
Mikaelrz <mikaeldioge20@gmail.com>
2022-07-06 11:50:35 -0300

    exercícios

---
+++ ExercíciosProfRomolo.MatemáticaBásica/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercíciosProfRomolo.MatemáticaBásica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Exercício 1 – Crie na solution um novo projeto com nome de ExerciciosProfRomulo.MatematicaBasica
+             * Exerícicio 2- Crie na solution um novo projeto com o nome de ExerciciosProfRomulo.EscolinhaTicTic
+             * Exercício 3- No projeto MatematicaBasica crie um Programa que calcule a soma de dois números inteiros. Considere que o usuário vai digitar esses dois números.
+             */
+            //declara as variáveis
+            int num1;
+            int num2;
+            int num3;
+            //entrada de dados
+            Console.WriteLine("jovem jovem digite dois números");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            soma = num1 = num2;
+            Console.WriteLine(soma);
+            
+
+            
+        }
+    }
+}
