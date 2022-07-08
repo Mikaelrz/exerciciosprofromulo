870c557d0ddc378797738ef7e25b3e93d89ea475
Mikaelrz <mikaeldioge20@gmail.com>
2022-07-08 08:58:49 -0300

    primeiro commit

--- ExercicioProfRomulo.ExercicioSeis/Program.cs
+++ ExercicioProfRomulo.ExercicioSeis/Program.cs
@@ -10,6 +10,32 @@ namespace ExercicioProfRomulo.ExercicioSeis
     {
         static void Main(string[] args)
         {
+            /*
+             * 
+             * Exercícios 6
+            Crie um algoritmo que mostre os 100 primeiros números pares.
+            Use
+            While
+            If
+            %
+             * 
+             * 
+             */
+            //1 a 100
+            int i;
+            i = 1;
+            while (i <= 100)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+                i++;
+            }
+            Console.ReadKey();
+
+
         }
     }
 }
