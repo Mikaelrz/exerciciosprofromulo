df6d0940478ffaa78b030921911a7d137e954d8b
Mikaelrz <mikaeldioge20@gmail.com>
2022-07-08 11:51:40 -0300

    atualizando

---
+++ ExercicioProfRomulo.ExecicioSete/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioProfRomulo.ExecicioSete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Exercício 7
+             Mostrar os números de 100 até 1 de forma decrescente*/
+
+            int i = 100;
+            while(i > 0)
+            {
+                Console.WriteLine(i);
+               // i = i - 1;
+               i--;
+
+            }
+           
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ ExercicioProfRomulo.ExercicioOito/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioProfRomulo.ExercicioOito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*Exercício 8
+             * 
+            Crie um programa que entre com um número inteiro
+            e mostre o fatorial desse número.
+            Exemplo:
+            5! = 120
+            5*4*3*2*1=  120
+            */
+
+            int fatorial = 5;
+            for (int n = 5; n <= 10; n++)
+            {
+                fatorial *= n;
+                Console.WriteLine(n + " fatorial= " + fatorial);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
